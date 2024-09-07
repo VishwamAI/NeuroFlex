9fa107dc6a4dd2e7a32f4abbe602da21d04b2246
swayampakula.v.s.s.pavanakasinadha sarma <81065703+kasinadhsarma@users.noreply.github.com>
2024-09-07 19:33:06 +0530

    Update __init__.py

--- NeuroFlex/__init__.py
+++ NeuroFlex/__init__.py
@@ -1,9 +1,12 @@
-__version__ = "0.1.3"
+__version__ = "0.2.0"  # Incremented version
 
+# Importing core components
 from .core_neural_networks import *
+from .advanced_models import *
+from .ai_ethics import *
+from .bci_integration import *  # Removed duplicate
+from .cognitive_architectures import *
 from .scientific_domains import *
 from .utils import *
 from .edge_ai import *
 from .quantum_neural_networks import *
-from .bci_integration import *
-
