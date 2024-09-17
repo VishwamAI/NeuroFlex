c07318d4c877b7c91799c3a562f554406151da4e
swayampakula.v.s.s.pavanakasinadha sarma <81065703+kasinadhsarma@users.noreply.github.com>
2024-09-17 09:21:51 +0530

    Update __init__.py

--- NeuroFlex/Transformers/__init__.py
+++ NeuroFlex/Transformers/__init__.py
@@ -1,1 +1,19 @@
 #comming with huge
+from .unified_transformer import (
+    UnifiedTransformer, 
+    get_unified_transformer, 
+    PyTorchUnifiedTransformer, 
+    JAXUnifiedTransformer, 
+    FlaxUnifiedTransformer, 
+    SonnetUnifiedTransformer
+)
+
+__all__ = [
+    'UnifiedTransformer',
+    'get_unified_transformer',
+    'PyTorchUnifiedTransformer',
+    'JAXUnifiedTransformer',
+    'FlaxUnifiedTransformer',
+    'SonnetUnifiedTransformer',
+    'RLTextAgent'
+]
